75512e9ac5e1374a975ea8eab8aadfdafe527f84
alexorear <alex.orear@gmail.com>
2018-09-05 12:21:08 -0500

    Add gradient fill to GradientAppBar.

--- lib/main.dart
+++ lib/main.dart
@@ -31,13 +31,31 @@ class GradientAppBar extends StatelessWidget {
   final String title;
   final double barHeight = 66.0;
 
+
   GradientAppBar(this.title);
 
   @override
-  Widget build(BuildContext content) {
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery
+    .of(context)
+    .padding
+    .top;
+    
     return new Container(
-      height: barHeight,
-      decoration: new BoxDecoration(color: Colors.blue),
+      padding: new EdgeInsets.only(top: statusBarHeight),
+      height: barHeight + statusBarHeight,
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: [
+            const Color(0xFF3366FF),
+            const Color(0xFF00CCFF),
+          ],
+          begin: const FractionalOffset(0.0, 0.0),
+          end:  const FractionalOffset(0.0, 1.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp
+        )
+      ),
       child: new Center(
         child: new Text(
           title,
